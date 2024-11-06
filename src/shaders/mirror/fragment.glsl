132c7f2b55be563f91df0aae2c5acf048e94dbac
kriss <krzysztof.podmokly@gmail.com>
2024-11-06 18:45:28 +0100

    Cleanup

--- src/shaders/mirror/fragment.glsl
+++ src/shaders/mirror/fragment.glsl
@@ -7,16 +7,12 @@ varying vec2 vUv;
 #include ../includes/perlinNoise.glsl
 
 void main() {
-  // vec3 uColorStart = vec3(.903, 0.840, 0.813);
-  // vec3 uColorEnd = vec3(0.0, 0.48, 0.73);
-
   vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime * 0.1));
 
   float strength = cnoise(vec3(displacedUv * 5., uTime * 0.2));
 
   float outerFlow = distance(vUv, vec2(0.5)) * 5.0 - 1.4;
   strength += outerFlow;
-
 
   // strength += step(0.3, strength) * 0.8;
 
